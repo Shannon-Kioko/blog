62f27b15065d3c7c4b724d71d3cda04e729da888
Shannon-Kioko <shannonkaluki@gmail.com>
2024-09-12 05:06:35 +0300

    Add: My Profile page, still in the works

--- lib/bloggy_web/router.ex
+++ lib/bloggy_web/router.ex
@@ -78,6 +78,7 @@ defmodule BloggyWeb.Router do
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    get "/users/:user_id/profile", MainPageController, :view
 
     live "/posts", PostLive.Index, :index
     live "/posts/new", PostLive.Index, :new
